b7bbae39b7ab51cd84178146809a1bf0ff24d7d6
Nyloner <nyloner.root@gmail.com>
2017-10-12 21:30:08 +0800

    update

--- dHash.py
+++ dHash.py
@@ -8,18 +8,18 @@ def hamming_dist(str1, str2):
     return sum([ch1 != ch2 for ch1, ch2 in zip(str1, str2)])
 
 
-def image_phash(filepath):
+def image_dhash(filepath):
     img = Image.open(filepath)
-    hash_value = imagehash.phash(img, hash_size=16)
+    hash_value = imagehash.dhash(img, hash_size=16)
     return str(hash_value)
 
 
 def search_similar_images(src_img_path, dir_path):
     result = []
-    src_phash_value = image_phash(src_img_path)
+    src_dhash_value = image_dhash(src_img_path)
     file_path_list = os.listdir(dir_path)
-    if 'phash_data.json' in file_path_list:
-        with open(dir_path + '/phash_data.json', 'r') as f:
+    if 'dhash_data.json' in file_path_list:
+        with open(dir_path + '/dhash_data.json', 'r') as f:
             hash_data = json.load(f)
     else:
         hash_data = {}
@@ -29,14 +29,14 @@ def search_similar_images(src_img_path, dir_path):
             continue
         img_path = dir_path + '/' + img
         if img_path in hash_data:
-            dest_phash_value = hash_data[img_path]
+            dest_dhash_value = hash_data[img_path]
         else:
-            dest_phash_value = image_phash(img_path)
-            hash_data[img_path] = dest_phash_value
-        img_dist = hamming_dist(src_phash_value, dest_phash_value)
+            dest_dhash_value = image_dhash(img_path)
+            hash_data[img_path] = dest_dhash_value
+        img_dist = hamming_dist(src_dhash_value, dest_dhash_value)
         if img_dist <= 35:
             result.append([img_path, img_dist])
-    with open(dir_path + '/phash_data.json', 'w') as f:
+    with open(dir_path + '/dhash_data.json', 'w') as f:
         json.dump(hash_data, f)
     result = sorted(result, key=lambda x: x[1])
     return result
